5bee81103b5b864a1e5988c76479ce8d67402031
toshihiro.imamura <imamaura.orts@gmail.com>
2020-02-05 21:57:41 +0900

    Lesson8.9.4終了

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 今村俊博 on 2020/02/05.
+//  Copyright © 2020 toshihiro.imamura. All rights reserved.
+//
+
+import Foundation
+
+struct  Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
--- Instagram/SettingViewController.swift
+++ Instagram/SettingViewController.swift
@@ -7,8 +7,56 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class SettingViewController: UIViewController {
+    
+    @IBOutlet weak var displayNameTextField: UITextField!
+
+    @IBAction func handleChangeButton(_ sender: Any) {
+        if let displayName = displayNameTextField.text{
+            
+            if displayName.isEmpty{
+                SVProgressHUD.showError(withStatus: "表示名を入力してください")
+                return
+            }
+            
+            let user = Auth.auth().currentUser
+            if let user = user {
+                let changeRequest = user.createProfileChangeRequest()
+                changeRequest.displayName = displayName
+                changeRequest.commitChanges{ error in
+                    if let error = error{
+                        SVProgressHUD.showError(withStatus: "表示名の変更に失敗しました")
+                        print("DEBUG_PRINT:" + error.localizedDescription)
+                        return
+                    }
+                    print("DEBUG_PRINT: [displayName = \(user.displayName!)]の設定に成功しました")
+                    
+                    SVProgressHUD.showError(withStatus: "表示名を変更しました")
+                }
+            }
+        }
+        
+        self.view.endEditing(true)
+    }
+
+    @IBAction func handleLogoutButton(_ sender: Any) {
+        try! Auth.auth().signOut()
+        
+        let loginViewerController = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+        self.present(loginViewerController!,animated: true, completion: nil)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        let user = Auth.auth().currentUser
+        if let user = user {
+            displayNameTextField.text = user.displayName
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
